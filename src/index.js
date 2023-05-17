7bc6750baf724ce006687fdebdb32f043b381c2a
VEVA <impresionesveva@gmail.com>
2023-05-17 16:04:30 -0600

    VALIDACION DE TARJETA

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
-console.log(validator);
+const boton = document.getElementById("boton");
+
+boton.addEventListener("click", () => {
+    var nombre = document.getElementById("nombre").value;
+    var tarjeta = document.getElementById("tarjeta").value;
+    var cvv = document.getElementById("cvv").value;
+    mostrarAlerta(validator.validarTarjeta(tarjeta));
+});
+
+function mostrarAlerta(bandera){
+    if (bandera){
+        alert ('Tarjeta Valida')
+    }
+    else {
+        alert ('Tarjeta Invalida')
+    }
+}
