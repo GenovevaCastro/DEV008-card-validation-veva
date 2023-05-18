1d8053e6c9997cb57a15bac918f995951ef4816b
VEVA <impresionesveva@gmail.com>
2023-05-18 15:30:09 -0600

    CAMBIOS CON TEST APROBADOS

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import validator from './validator.js';
 const boton = document.getElementById("boton");
 
 boton.addEventListener("click", () => {
-    //var nombre = document.getElementById("nombre").value;
-    var tarjeta = document.getElementById("tarjeta").value;
-    //var cvv = document.getElementById("cvv").value;
-    mostrarAlerta(validator.validarTarjeta(tarjeta));
 
-    document.getElementById("tarjeta").value = validator.maskify(tarjeta);
+  const tarjeta = document.getElementById("tarjeta").value;
+  
+  mostrarAlerta(validator.isValid(tarjeta));
+
+  document.getElementById("tarjeta").value = validator.maskify(tarjeta);
 
 });
 
 function mostrarAlerta(bandera) {
-    if (bandera) {
-        alert('Tarjeta Valida')
-    }
-    else {
-        alert('Tarjeta Invalida')
-    }
+  if (bandera) {
+    alert('Tarjeta Valida')
+  }
+  else {
+    alert('Tarjeta Invalida')
+  }
 }
 
