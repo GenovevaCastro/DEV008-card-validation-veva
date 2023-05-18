e33f6b95adf0ad7bbe210321afa98d375b95915b
VEVA <impresionesveva@gmail.com>
2023-05-17 20:37:58 -0600

    se comentan lineas

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import validator from './validator.js';
 const boton = document.getElementById("boton");
 
 boton.addEventListener("click", () => {
-    var nombre = document.getElementById("nombre").value;
+    //var nombre = document.getElementById("nombre").value;
     var tarjeta = document.getElementById("tarjeta").value;
-    var cvv = document.getElementById("cvv").value;
+    //var cvv = document.getElementById("cvv").value;
     mostrarAlerta(validator.validarTarjeta(tarjeta));
 
     document.getElementById("tarjeta").value = validator.maskify(tarjeta);
